2728d78ad6fa80b700b7b1d831b17130800d60d5
irivintiranjit <irivinti.ranjit@gmail.com>
2021-06-02 23:38:50 -0400

    Enabled the CORS for API

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ app.get("/api/", (req, res) => {
   res.send("Node JS app Started");
 });
 
+/*Registering for CORS  */
+app.all("*", (req, res) => {
+  res.header("Access-Control-Allow-Orgin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTION");
+  next();
+});
+/*Registering to read the request bodt in json format */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/*Registering the routes */
 app.use("/api/auth", AuthController);
 app.use("/api/user", userRouter);
 
